40afee7276af6bf98ef5702f542d96f6bec1d8f6
Wilson Júnior <wilsonpjunior@gmail.com>
2017-01-31 17:53:38 -0200

    Improve event script to handle mongo 3.4

--- mongodb-cluster-join.sh
+++ mongodb-cluster-join.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e
-
 hostname=$1
 LOG=/var/log/all
+
+echo "Start mongo event for" $1 "Suffix:" $MONGODB_SUFFIX >> $LOG
 
 
 echo $hostname | grep -q '^rs'
@@ -15,14 +15,24 @@ if [ $? == 0 ]; then
         db_number=`echo $hostname | sed 's|\..*||g;s|.*db||g'`
 
         if [ $db_number == 01 ]; then
-            mongo ${hostname}:27017/test --eval "JSON.stringify(rs.initiate());" >> $LOG
+            echo "Start initializing the replicaset" $hostname
+            mongo ${hostname}:27017/admin --eval "JSON.stringify(rs.initiate());" >> $LOG 2>&1
             sleep 5
-            mongo router01:27017/test --eval "JSON.stringify(sh.addShard('rs${rs_number}/${hostname}:27017'))" >> $LOG
+            echo "Start add the created replicaset into sharding" $hostname
+            mongo router01.${MONGODB_SUFFIX}:27017/admin --eval "JSON.stringify(sh.addShard('rs${rs_number}/${hostname}:27017'))" >> $LOG 2>&1
             sleep 2
         else
-            primary="rs${rs_number}db01"
-            mongo ${primary}:27017/test --eval "JSON.stringify(rs.add('$hostname:27017'))" >> $LOG
+            echo "Start add the host into replicaset" $hostname
+            primary="rs${rs_number}db01.${MONGODB_SUFFIX}"
+            mongo ${primary}:27017/admin --eval "JSON.stringify(rs.add('$hostname:27017'))" >> $LOG 2>&1
         fi
 
     ) 200>/var/lock/.mongodb-cluster.lock
+fi
+
+echo $hostname | grep -q '^configserver01'
+if [ $? == 0 ]; then
+    echo "Setup config server replicaset" >> $LOG
+    sleep 5
+    mongo ${hostname}:27019/admin --eval "JSON.stringify(rs.initiate({_id: 'cfg', configsvr: true, members: [{_id:0, host:'configserver01.${MONGODB_SUFFIX}:27019'}, {_id:1, host:'configserver02.${MONGODB_SUFFIX}:27019'}, {_id: 2, host: 'configserver03.${MONGODB_SUFFIX}:27019'}]}));" >> $LOG 2>&1
 fi
